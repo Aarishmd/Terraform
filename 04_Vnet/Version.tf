cd84f5cb2213be70bd4526be2826f47dc2179f5a
aarishmd <aarishmd12@gmail.com>
2024-10-13 23:39:36 +0530

    Terraform for Vnet

---
+++ 04_Vnet/Version.tf
@@ -0,0 +1,21 @@
+#terraform
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+   azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0"  # specify the desired version
+    } 
+    random ={
+     source = "hashicorp/random"
+     version = ">=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+   subscription_id = "92882f0b-81c9-4214-8f4b-115816a1938a"
+   features {
+      
+   }
+}
