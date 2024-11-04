5ba9fabc88e4f443ad31741b41e058892d840b78
aarishmd <aarishmd12@gmail.com>
2024-11-04 15:36:24 +0530

    VMSS auto scaling

---
+++ 09_VMSS_AutoScaling/linuxvm-network-interface.tf
@@ -0,0 +1,13 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "${local.resource_name_prefix}-nic"
+  resource_group_name = azurerm_resource_group.MyRG.name
+  location            = azurerm_resource_group.MyRG.location
+
+  ip_configuration {
+    name                          = "web-linuxvm-ip-1"
+    subnet_id                     = azurerm_subnet.web_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+
+}
